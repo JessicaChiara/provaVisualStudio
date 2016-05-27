c3362968b6d21927d6a4ab007081a126aca66d4a
JessicaChiara <jessicacatania90@gmail.com>
2016-05-27 17:49:40 +0200

    Update App.cs

--- vediamoIlRepos/vediamoIlRepos/vediamoIlRepos/App.cs
+++ vediamoIlRepos/vediamoIlRepos/vediamoIlRepos/App.cs
@@ -20,7 +20,7 @@ namespace vediamoIlRepos
                     Children = {
                         new Label {
                             XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Jessicaaaaaaa!"
+                            Text = "Welcome to Xamarin ehi!"
                         }
                     }
                 }
